1b75e1eceec741e9433fec028040c63209469e7e
Steffen Geving <79884124+GEVIS1@users.noreply.github.com>
2021-09-20 17:26:23 +1200

    Add class work

--- node-test/app.js
+++ node-test/app.js
@@ -2,30 +2,42 @@
  * Config section
  *
  */
+const { notStrictEqual } = require('assert');
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.set("view engine", "ejs");
+app.use(expressLayouts)
+app.use(express.static(path.join(__dirname, "assets")))
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 /*
  * Controller section
  *
  */
 
-
 app.get('/', (req, res) => {
-    let input = "";
+    let dataObject = {  name : null,
+                        staticString : "The most static string in the land version 2" }
     if (req.query.name != null){
-        input = req.query.name;
+        dataObject.name = req.query.name;
     }
+    
+    res.render('index', dataObject)
+});
 
-    res.send(`Hello World! <br> You gave me some input: ${input}`)});
 
 app.get('/secret', (req, res) => res.send(`You're not meant to see this!!! ${req.url} <br>`))
-app.get('/nevergonnagiveyouup', (res) => res.send(`<marquee>${imageTag}</marquee>`))
-app.get('/google', (res) => res.redirect(`http://www.google.com`))
+app.get('/nevergonnagiveyouup', (req, res) => {
+    let imageTag = `<img src="${"img/astley.jpg"}" alt="Never gonna give you up!">`;
+    res.send(`<marquee>${imageTag}</marquee> <h1>${__dirname}</h1>`);
+})
+app.get('/google', (req, res) => res.redirect(`http://www.google.com`))
+//express.bind
 
 app.get('/foo', (req, res) => {
     res.redirect(`/bar`)
